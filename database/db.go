9e939b854e7afb75505b767fce8f1be90914c619
yurarara23 <urauraura2323@gmail.com>
2026-02-17 16:47:27 +0900

    修正

--- database/db.go
+++ database/db.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "root:password@tcp(127.0.0.1:3306)/nu_db?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:password@tcp(db:3306)/nu_db?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
